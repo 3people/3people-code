74a3449a0be135c49707f6163ea60a744ee7cf74
3people <smsn7031@naver.com>
2016-12-15 01:21:31 +0900

    day8

--- day8/skel 2/sortlines/qsortint.c
+++ day8/skel 2/sortlines/qsortint.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 /* swap: interchange v[i] and v[j] */
-void swap(int v[], int i, int j) {
-	int temp;
+void swap(char *v[], int i, int j) {
+	char *temp;
 
 	temp = v[i];
 	v[i] = v[j];
@@ -10,33 +11,30 @@ void swap(int v[], int i, int j) {
 }
 
 /* qsort: sort v[left]...v[right] into increasing order */
-void qsort(int v[], int left, int right) {
-	int i, last;
+void qsort(char *v[], int left, int right) {
+	int i, last, pivot;
 
 	if (left >= right)
 		return;
 	swap(v, left, (left + right)/2);
 	pivot = left;
 	for (i = left+1; i <= right; i++)
-		if (v[i] < v[left])
+		if (strcmp(v[i], v[left]) < 0)
 			swap(v, ++pivot, i);
 	swap(v, left, pivot);
 	qsort(v, left, pivot-1);
 	qsort(v, pivot+1, right);
 }
 
-int main() {
-	int a[10] = {5,2,4,3,6,8,7,1,9,10};
-	int i;
-
-	for (i = 0; i < 10; i++)
-		printf("%d ", a[i]);
-	printf("\n");
-	qsort(a, 0, 9);
-	for (i = 0; i < 10; i++)
-		printf("%d ", a[i]);
-	printf("\n");
-}
-
-
-
+// int main() {
+// 	int a[10] = {5,2,4,3,6,8,7,1,9,10};
+// 	int i;
+//
+// 	for (i = 0; i < 10; i++)
+// 		printf("%d ", a[i]);
+// 	printf("\n");
+// 	qsort(a, 0, 9);
+// 	for (i = 0; i < 10; i++)
+// 		printf("%d ", a[i]);
+// 	printf("\n");
+// }
